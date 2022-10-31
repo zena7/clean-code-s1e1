442093fcfebc6004e0f9963c3a03f9c19c5db85e
Nataly Zernova <nz_18@mail.ru>
2022-10-31 23:34:36 +0300

    refactor: customize semantics according to thq 1.1 rules

--- app.js
+++ app.js
@@ -8,8 +8,8 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
+var taskInput=document.querySelector(".new-task");//Add a new task.
+var addButton=document.querySelector(".querySelector");//first button
 var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
